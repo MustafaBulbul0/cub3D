df0287b243f0140114fea5ccd912f1b605dc0996
Mustafa Bülbül <bulbulmustafa422@gmail.com>
2025-09-14 18:26:39 +0300

    add

--- srcs/game/key_press.c
+++ srcs/game/key_press.c
@@ -4,10 +4,13 @@ static void	move_forward(t_all *all)
 {
 	double	new_x;
 	double	new_y;
+	double	rad;
 
-	new_x = all->game->player_x + cos(all->game->player_angle) * MOVE_SPEED;
-	new_y = all->game->player_y + sin(all->game->player_angle) * MOVE_SPEED;
-	if (all->game->map[(int)new_y] && 
+	rad = all->game->player_angle * Y_PI / 180;
+	new_x = all->game->player_x + cos(rad) * MOVE_SPEED;
+	new_y = all->game->player_y + sin(rad) * MOVE_SPEED;
+	if ((int)new_y >= 0 && all->game->map[(int)new_y] &&
+		(int)new_x >= 0 && (int)new_x < (int)ft_strlen(all->game->map[(int)new_y]) &&
 		all->game->map[(int)new_y][(int)new_x] != '1')
 	{
 		all->game->player_x = new_x;
@@ -19,10 +22,13 @@ static void	move_backward(t_all *all)
 {
 	double	new_x;
 	double	new_y;
+	double	rad;
 
-	new_x = all->game->player_x - cos(all->game->player_angle) * MOVE_SPEED;
-	new_y = all->game->player_y - sin(all->game->player_angle) * MOVE_SPEED;
-	if (all->game->map[(int)new_y] && 
+	rad = all->game->player_angle * Y_PI / 180;	
+	new_x = all->game->player_x - cos(rad) * MOVE_SPEED;
+	new_y = all->game->player_y - sin(rad) * MOVE_SPEED;
+	if ((int)new_y >= 0 && all->game->map[(int)new_y] &&
+		(int)new_x >= 0 && (int)new_x < (int)ft_strlen(all->game->map[(int)new_y]) &&
 		all->game->map[(int)new_y][(int)new_x] != '1')
 	{
 		all->game->player_x = new_x;
@@ -34,10 +40,13 @@ static void	move_left(t_all *all)
 {
 	double	new_x;
 	double	new_y;
+	double	rad;
 
-	new_x = all->game->player_x - sin(all->game->player_angle) * MOVE_SPEED;
-	new_y = all->game->player_y + cos(all->game->player_angle) * MOVE_SPEED;
-	if (all->game->map[(int)new_y] && 
+	rad = (all->game->player_angle - 90) * Y_PI / 180;
+	new_x = all->game->player_x + cos(rad) * MOVE_SPEED;
+	new_y = all->game->player_y + sin(rad) * MOVE_SPEED;
+	if ((int)new_y >= 0 && all->game->map[(int)new_y] &&
+		(int)new_x >= 0 && (int)new_x < (int)ft_strlen(all->game->map[(int)new_y]) &&
 		all->game->map[(int)new_y][(int)new_x] != '1')
 	{
 		all->game->player_x = new_x;
@@ -49,10 +58,13 @@ static void	move_right(t_all *all)
 {
 	double	new_x;
 	double	new_y;
+	double	rad;
 
-	new_x = all->game->player_x + sin(all->game->player_angle) * MOVE_SPEED;
-	new_y = all->game->player_y - cos(all->game->player_angle) * MOVE_SPEED;
-	if (all->game->map[(int)new_y] && 
+	rad = (all->game->player_angle + 90) * Y_PI / 180;
+	new_x = all->game->player_x + cos(rad) * MOVE_SPEED;
+	new_y = all->game->player_y + sin(rad) * MOVE_SPEED;
+	if ((int)new_y >= 0 && all->game->map[(int)new_y] &&
+		(int)new_x >= 0 && (int)new_x < (int)ft_strlen(all->game->map[(int)new_y]) &&
 		all->game->map[(int)new_y][(int)new_x] != '1')
 	{
 		all->game->player_x = new_x;
